41382ba87162007c4730cb1bb2234122e6c9ea12
CaribouW <1071956678@qq.com>
2019-11-18 10:04:31 +0800

    add:添加模型

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # For-Git-Commit
-Git commit learning based on RNN
+Git commit learning based on RNN<br>
+_打工仔在线卑微_
--- model/sample.py
+++ model/sample.py
@@ -1,2 +1,9 @@
+import os
+
 if __name__ == '__main__':
-    pass
+    path = "download"
+    dirs = os.listdir(path)
+
+    # 输出所有文件和文件夹
+    for file in dirs:
+        print(file)
